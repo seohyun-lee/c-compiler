0a4a2d8d0f6a7415b1c690925791d34c477e936f
seohyun-lee <applemint2003@gmail.com>
2025-06-03 01:52:25 +0900

    과제3 작성중

---
+++ Project54/Project54/report_error.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char* yytext;
+
+void print_error(int err_num)
+{
+	switch (err_num)
+	{
+	case 1:
+		/* start with digit */
+		printf("Error - Start with digit (%s)\n", yytext);
+		break;
+	case 2:
+		printf("Error - long identifier (%s)\n", yytext);
+		break;
+	case 3:
+		printf("Error - Invalid character (%s)\n", yytext);
+		break;
+	default:
+		printf("Error - Unknown\n");
+		break;
+	}
+
+}
